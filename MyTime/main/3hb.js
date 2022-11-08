be2201f4ac40fb91c07426c5bd55ed91feb04366
HatimBlack <117414450+HatimBlack@users.noreply.github.com>
2022-11-08 07:36:42 +0100

    Add files via upload

---
+++ MyTime/main/3hb.js
@@ -0,0 +1,8 @@
+let bce = document.getElementById('bce');
+window.onscroll = function(){
+    let value = scrollY;
+    console.log(value)
+
+}
+
+ bce.innerHTML += '<p>hello</p>'
---
+++ MyTime/main/log.js
@@ -0,0 +1,6 @@
+let bce = document.getElementById('bce');
+window.onscroll = function(){
+    let value = scrollY;
+    console.log(value)
+
+}
